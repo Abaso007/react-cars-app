d0e57ba4e664d162d599c02eaf4cccdb190bcf4c
ICEPrey <abuwswork@gmail.com>
2021-08-22 19:35:00 -0700

    Landing Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-// tailwind.config.js
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
@@ -6,9 +5,19 @@ module.exports = {
     extend: {},
     screens: {
       sm: "640px",
-      md: "760px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
       lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
       xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
     },
   },
   variants: {
